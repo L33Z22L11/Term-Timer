455a4ef7cce3b1a94bf6950080c0406884a86d00
Zhilu <hi@zhilu.cyou>
2021-05-29 07:14:06 +0800

    Update nextexam.js

--- nextexam.js
+++ nextexam.js
@@ -1,18 +1,19 @@
-﻿onload = change("高二理科");
+﻿onload = function() {
+    change("高二理科");
+    String(location).indexOf("\?") == -1 ? updateClock() : check();
+ }
 
 oncontextmenu = onkeydown = onselectstart = function () { return false; }
 
 function change(i) {
-    now = new Date("2021-05-28T13:45");
+    now = new Date("2021-05-28T13:00");
     end = 0;
     progress = 0;
     order = 0;
-    tell = ["电脑时间存在误差，仅供参考，请以实际铃声为准。"];
 
     type = i;
     console.log(type);
     output("type", type);
-    String(location).indexOf("\?") == -1 ? updateClock() : check();
 }
 
 function updateClock() {
@@ -54,6 +55,7 @@ function output(id, value) {
 }
 
 function examTimer() {
+    tell = ["电脑时间存在误差，仅供参考，请以司号和广播通知为准。"];
     switch (type) {
         case "高二理科":
             $("英语", "2021-05-28T14:20", "2021-05-28T16:20");
@@ -72,6 +74,7 @@ function examTimer() {
             $("历史", "2021-05-29T16:50", "2021-05-29T18:30");
             break;
         case "高一":
+            tell = ["电脑时间存在误差，仅供参考，请以实际铃声为准。"];
 //             $("未知", "2021-05-28T07:50", "2021-05-28T09:50");
 //             $("未知", "2021-05-28T10:20", "2021-05-28T12:00");
             $("物理", "2021-05-28T14:20", "2021-05-28T16:00");
@@ -85,6 +88,9 @@ function examTimer() {
     duration = getClock(start) + "~" + getClock(end);
 
     if (now < (start - 18E5)) {
+        tell = ["发千古之幽思，励宏图之大志。",
+            "冲，宝中干饭人！",
+            "今天咥碗臊子面，嘹咋咧！"];
         timer = "Soon";
         next = "考试加油";
         progress = 0;
@@ -109,10 +115,11 @@ function examTimer() {
         next = "距离结束";
         progress = (now - start) / (end - start) * 100;
     } else {
-        tell = ["本页面托管在纸鹿小站上",
+        tell = ["本页面由纸鹿小站托管",
             "原服务器无法访问，我们已紧急迁移到新服务器",
             "目前我们可以维持服务器开支",
-            "精神赞助可联系 QQ 2399052066"];
+            "合作/技术交流/扩列 QQ 2399052066",
+            "野生技协现已开放加入，群 894656456"];
         subject = "";
         duration = "";
         timer = "";
